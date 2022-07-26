ce87b4f7f71f0a5d0a9cc6dc9ef450f0f09ada5e
PatrickARatcliff <pratcliff5@gmail.com>
2022-07-25 18:42:35 -0600

    change 'if' statament, add id for each row

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,6 +6,14 @@ $(console.log(currentHour));
 var inputField;
 //var input = $(document.getElementsByTagName.forEach("input"));
 var input09 = $("#input09");
+var input10 = $("#input10");
+var input11 = $("#input11");
+var input12 = $("#input12");
+var input13 = $("#input13");
+var input14 = $("#input14");
+var input15 = $("#input15");
+var input16 = $("#input16");
+var input17 = $("#input17");
 //document.getElementById("#input10");
 //document.getElementById("#input11");
 //document.getElementById("#input12");
@@ -14,6 +22,8 @@ var input09 = $("#input09");
 //document.getElementById("#input15");
 //document.getElementById("#input16");
 //document.getElementById("#input17");
+//var time =$("#hour09");
+//console.log(time);
 
 
 //var time = document.querySelector(input());
@@ -21,16 +31,15 @@ $(document).ready(function() {
     //var inputRowTime = $(input.data("time"));
     //console.log(inputRowTime);
     //for (let index = 0; index < inputRowTime.length; index++) {
-        //var time
-        if (time === currentHour) {
+        if (09 === currentHour) {
             $(input09.addClass("present"));
         }
-        else if (time < currentHour) {
-                $(input09.addClass("past"));
-            } 
-            else {
-                $(input09.addClass("future"));
-            }
+        else if (09 < currentHour) {
+            $(input09.addClass("past"));
+        } 
+        else {
+            $(input09.addClass("future"));
+        }
         
    // };
               
